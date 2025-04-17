82db41a0853bd712945067cb6358a1caa8a2c027
jasonyu0705 <49047194+jasonyu0705@users.noreply.github.com>
2025-04-17 13:24:18 -0700

    first push

---
+++ Project_1/Project1.py
@@ -0,0 +1,37 @@
+import sys # in order to access command line prompts
+
+
+
+
+
+def main():
+    testing= False
+    # if statement that just makes testing easier
+    if testing == True:
+        algorithm = "depth-first"
+        file = "text-file.txt"
+    if testing == False:
+        if len(sys.argv) != 3:
+            print("Usage: python3 planner.py [algorithm] [world-file]")
+            sys.exit(1)
+        algorithm = sys.argv[1]
+        file = sys.argv[2]
+    # this is sorta just all parcing data to be ready for the search
+    #reading file
+    with open(file, "r") as f:
+        colNum = int(f.readline().strip())
+        rowNum = int(f.readline().strip())
+        grid = [list(f.readline().strip()) for i in range(rowNum)]
+    
+    # robotPos = None
+    robotPos
+    dirtyTiles = set()
+
+    for r in range(len(grid)):
+        for c in range(len(grid[r])):
+            if grid[r][c] == '@':
+                robotPos = (r, c)
+            elif grid[r][c] == '*':
+                dirtyTiles.add((r, c))
+
+    #from here set initial position and then you can choose whether to go into dfs and bfs, which will be functions 
